efca44d3314684c46a7b68238195e734f028367f
parv-nangalia <nangaliaparv@gmail.com>
2025-03-12 15:04:07 +0530

    Required urls

--- backend/gurukul/urls.py
+++ backend/gurukul/urls.py
@@ -13,10 +13,61 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('schedule/', views.schedule, name='schedule'),
-    path('gurukul/<int:id>', views.class_details, name='gurukul-details'),
-    path('gurukul/<int:id>/feed', views.class_feed, name='class-feed'),
-    path('gurukul/<int:id>/learning', views.class_learnings, name='class-learning'),
-    path('gurukul/<int:id>/schedule', views.class_schedule, name='class-schedule'),
+    path('notification/', views.UserNotificationView.as_view(), name='notification'),
+
+    path('gurukul/', views.GurukulView.as_view(), name='gurukul'),
+    path('gurukul/join/', views.JoinGurukul.as_view(), name='join-gurukul'),
+    path('gurukul/<int:id>/', views.GurukulDetailView.as_view(), name='gurukul-details'),
+    path('gurukul/<int:id>/leave/', views.LeaveGurukulView.as_view(), name='leave-gurukul'),
+#   path('gurukul/<int:id>', views.class_details, name='gurukul-details'),
+    path('gurukul/<int:id>/feed/', views.FeedView.as_view(), name='class-feed'),
+    path('gurukul/<int:id>/learning/', views.class_learnings, name='class-learning'),
+    path('gurukul/<int:id>/schedule/', views.class_schedule, name='class-schedule'),
+
+    path('gurukul/<int:id>/question/', views.QuestionView.as_view(), name='class-question'),
+    path('gurukul/<int:id>/question/<int:question_id>', views.QuestionView.as_view(), name='question-object'),
+   
+    path('gurukul/<int:id>/announcement/', views.AnnouncementView.as_view(), name= 'class-announcements'),
+    path('gurukul/<int:id>/announcement/<int:announcement_id>/', views.AnnouncementsDetailView.as_view(), name='class-announcement-object'),
+    
+    path('gurukul/<int:id>/activity/', views.RecentActivityView.as_view(), name='gurukul-activity'),
+
+    path('learningPath/', views.LearningPathView.as_view(), name='learning-path'),
+    path('learningPath/publish/', views.PublishLearningPath.as_view(), name='publish-learningpath'),
+    path('learningPath/<int:id>/', views.LearningPathDetailView.as_view(), name='learningPath_detail'),
+    path('learningPath/<int:learningpath_id>/progress/', views.LearningPathProgressView.as_view(), name='learningpath-progress'),
+    path('learingpPath/<int:learningpath_id>/complete/',views.LearningPathCompleteView.as_view(), name='learninpath-complete'),
+    
+    path('assignment/', views.AssignmentView.as_view(), name='assignment'),
+    path('assignment/<int:assignment_id>/', views.AssignmentDetailView.as_view(), name='assignment_details'),
+    path('assignment/<int:assignment_id>/submission/', views.AssignmentSubmissionView.as_view(), name='assignment-submission'),
+    path('assignment/<int:assignment_id>/submission/<int:submission_id>/', views.AssignmentSubmissionDetailView.as_view(), name='submission-detail'),
+
+#  gurukul.gyaandweep.com/gurukul/"guruk_uuid"/quiz/"quiz_uuid"
+#  gurukul.gyaandweep.com/gurukul/""guruku_uuid/leaningpath/"ksabdks"/
+
+    #js all the inside components
+    #    gurukul.gyaandweep.com/learningpath/akjwdn/module/
+
+#  gurukul.gyaandweep.com/gurukul/"guruk_uuid"/module/"module_uuid"/shloka/"shlokaa_uuid"
+
+
+
+    path('quiz/', views.QuizView.as_view(), name='quiz'),
+    path('quiz/<int:quiz_id>/', views.QuizDetailView.as_view(), name='quiz_detail'),
+    path('quiz/<int:quiz_id>/submission/', views.QuizSubmissionView.as_view(), name='quiz-overview'),
+    # path('quiz/<int:quiz_id>/overview/', views.QuizOver)
+    
+    path('test/', views.TestView.as_view(), name='test'),
+    path('test/<int:test_id>/', views.TestDetailView.as_view(), name='test_details'),
+    path('test/<int:test_id>/submission/', views.QuizSubmissionView.as_view(), name='quiz-overview'),
+
+    
+    path('studyMaterial/', views.StudyMaterialView.as_view(), name='studymaterial'),
+    path('studyMaterial/<int:studymaterial_id>/', views.StudyMaterialDetailView.as_view(), name='studymaterial_details'),
+
+    path('module/', views.ModuleView.as_view(), name='modules'),
+    path('module/<int:id>/', views.ModuleDetailView.as_view(), name = 'modules_details'),
 
     path('template/', views.template, name='guru-templates'),
     path('template/learningpaths', views.learningpathslist, name='guru-learningpaths'),
